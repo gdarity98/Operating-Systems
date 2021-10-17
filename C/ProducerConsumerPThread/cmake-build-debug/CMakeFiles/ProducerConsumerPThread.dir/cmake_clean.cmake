352fa576e90a95e0b464dab7b1f3f0a403864ab4
Gabe Darity <38188352+gdarity98@users.noreply.github.com>
2021-10-16 23:21:28 -0600

    Add java and c assignments
    adding paging assignment
    adding producer\consumer problem assignment

---
+++ C/ProducerConsumerPThread/cmake-build-debug/CMakeFiles/ProducerConsumerPThread.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ProducerConsumerPThread.dir/main.c.obj"
+  "ProducerConsumerPThread.exe"
+  "ProducerConsumerPThread.exe.manifest"
+  "ProducerConsumerPThread.pdb"
+  "libProducerConsumerPThread.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ProducerConsumerPThread.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ C/ProducerConsumerPThread/cmake-build-debug/CMakeFiles/ProducerConsumerPThread.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for ProducerConsumerPThread.
+# This may be replaced when dependencies are built.
